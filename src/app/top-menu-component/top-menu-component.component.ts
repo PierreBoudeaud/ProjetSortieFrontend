5a7cd584f93c537d0ef4352d019ea285400083b6
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-10 12:24:21 +0200

    Add toaster

--- src/app/top-menu-component/top-menu-component.component.ts
+++ src/app/top-menu-component/top-menu-component.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ConnexionService } from '../services/connexion.service';
+import {ToasterService} from 'angular2-toaster';
 
 @Component({
   selector: 'app-top-menu-component',
+  providers: [ToasterService],
   templateUrl: './top-menu-component.component.html',
   styleUrls: ['./top-menu-component.component.css']
 })
 export class TopMenuComponent implements OnInit {
+  private static connexionService: ConnexionService;
+  public static toasterService: ToasterService;
+
+  constructor(public auth: ConnexionService, private toasterService: ToasterService) {
+    TopMenuComponent.toasterService = this.toasterService;
+    TopMenuComponent.connexionService = this.auth;
+   }
+
+  isAuth(): boolean {
+    return ConnexionService.isAuth;
+  }
 
 
-  constructor() { }
+  public static popToastSuccess(title: string, body: string) {
+    this.popToast('success', title, body);
+  }
 
-  ngOnInit() {
+  public static popToastError(title: string, body: string) {
+    this.popToast('error', title, body);
+  }
+
+  private static popToast(type: string, title: string, body: string) {
+    this.toasterService.pop(type, title, body);
   }
 
 }
