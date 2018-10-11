2b41a21e7fe11de467c99d58f864c400a3777be0
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-11 16:07:54 +0200

    Adapt creation sortie component to create and update

--- src/app/creation-sortie/creation-sortie.component.ts
+++ src/app/creation-sortie/creation-sortie.component.ts
@@ -11,6 +11,7 @@ import { Ville } from '../models/ville.model';
 import { Lieu } from '../models/lieu.model';
 import { LieuService } from '../services/lieu.service';
 import { Site } from '../models/site.model';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-creation-sortie',
@@ -26,18 +27,23 @@ export class CreationSortieComponent implements OnInit {
   public sortie: Sortie;
   public villes: Ville[];
   public site: Site;
+  public updatedSortie: Sortie = null;
+
+  public villeIndex: number;
+  public lieuIndex: number;
+
   constructor(private participantService: ParticipantService,
     private sortieService: SortieService, private villeService: VilleService,
-    private lieuService: LieuService) { }
+    private lieuService: LieuService, private route: ActivatedRoute) {
+      this.sortie = new Sortie();
+      this.ville = new Ville();
+      this.lieu = new Lieu();
+      this.participant = new Participant();
+     }
 
   ngOnInit() {
     this.getParticipant();
     this.getVille();
-    this.getLieux();
-    this.sortie = new Sortie();
-    this.ville = new Ville();
-    this.lieu = new Lieu();
-    this.participant = new Participant();
   }
 
   getParticipant() {
@@ -71,20 +77,67 @@ export class CreationSortieComponent implements OnInit {
     this.villeService.getVilles().subscribe(
       data => {this.villes = data; console.log(data); },
       err => console.error(err),
-      () => console.log('villes récupérées')
+      () =>  {
+        console.log('villes récupérées');
+        const idSortie = this.route.snapshot.paramMap.get('id');
+        if (idSortie !== null) {
+          this.sortieService.getSortie(idSortie).toPromise()
+            .then(sortie => {
+              this.updatedSortie = sortie;
+              this.sortie.id = sortie.id;
+              this.sortie.nom = sortie.nom;
+              this.sortie.etat = sortie.etat;
+              this.sortie.duree = sortie.duree;
+              this.sortie.description = sortie.description;
+              this.sortie.dateDebut = new Date(sortie.dateDebut);
+              this.sortie.dateCloture = new Date(sortie.dateCloture);
+              this.sortie.nbInscriptions = sortie.nbInscriptions;
+              this.sortie.organisateur = sortie.organisateur;
+              let i = 0;
+              let found = false;
+              this.villes.forEach(ville => {
+                if (!found && ville.id !== sortie.lieu.ville.id) {
+                  i++;
+                } else {
+                  found = true;
+                }
+              });
+              this.changeVille(i);
+              this.villeIndex = i;
+            })
+            .catch();
+        } else {
+          this.getLieux(this.villes[0].id);
+        }
+     }
     );
   }
 
-  getLieux() {
-    this.lieuService.getLieux().subscribe(
+  getLieux(idVille: number) {
+    this.lieuService.getLieuOfVille(idVille).subscribe(
       data => {this.lieux = data; console.log(data); console.log(this.lieux); },
       err => console.error(err),
-      () => console.log('lieux réupérées')
+      () => {
+        console.log('lieux récupérées');
+        if (this.updatedSortie !== null) {
+          let index: number;
+          let found = false;
+          this.lieux.forEach(lieu => {
+            if (!found && lieu.id !== this.updatedSortie.lieu.id) {
+              index++;
+            } else {
+              found = true;
+            }
+          });
+          this.showDetailLieu(index);
+          this.lieuIndex = index;
+        }
+      }
     );
   }
 
   showDetailLieu(index) {
-    const lieu = this.lieux[index];
+    const lieu = index > -1 ? this.lieux[index] : new Lieu();
     console.log(lieu);
     this.lieu.id = lieu.id;
     this.lieu.nom = lieu.nom;
@@ -94,6 +147,16 @@ export class CreationSortieComponent implements OnInit {
     this.lieu.adresse = lieu.adresse;
     this.lieu.latitude = lieu.latitude;
     this.lieu.longitude = lieu.longitude;
+  }
+
+  changeVille(index) {
+    const ville = this.villes[index];
+    console.log(ville);
+    this.ville.id = ville.id;
+    this.ville.nom = ville.nom;
+    this.ville.codePostal = ville.codePostal;
+    this.showDetailLieu(-1);
+    this.getLieux(ville.id);
   }
 
 }
