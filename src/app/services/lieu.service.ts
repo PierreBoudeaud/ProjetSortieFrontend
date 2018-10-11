1521e33142df5370022343ec1fbf043379c3943e
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-11 16:07:24 +0200

    Update lieu service

--- src/app/services/lieu.service.ts
+++ src/app/services/lieu.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { PathService } from './path.service';
 import { Observable } from 'rxjs';
 import { Lieu } from '../models/lieu.model';
@@ -17,5 +17,11 @@ export class LieuService {
 
     getLieu(id: String): Observable<Lieu> {
         return this.http.get<Lieu>(LieuService.path + id);
+    }
+
+    getLieuOfVille(idVille: number): Observable<Lieu[]> {
+        let param = new HttpParams();
+        param = param.append('ville', idVille.toString());
+        return this.http.get<Lieu[]>(LieuService.path, { params: param });
     }
 }
